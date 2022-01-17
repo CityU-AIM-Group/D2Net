9bd01b840fc26b7f52ee0d329e0b7ea622001211
Qiushi Yang <mryangqiushi@gmail.com>
2022-01-17 16:29:57 +0800

    Delete train.sh

--- train.sh
+++
@@ -1,7 +0,0 @@
-python train.py  --gpu=0 --net=DisenNet --batch_size=1 \
-    --valid_freq=5 --output_set=train_val \
-    --DisenNet_indim=8 --AuxDec_dim=4 \
-    --miss_modal=True --use_Bernoulli_train=True \
-    --use_contrast=True --use_freq_contrast=True \
-    --use_distill=True --use_kd=True --affinity_kd=True \
-    --setting=D2Net 
